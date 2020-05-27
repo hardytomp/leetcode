69caf655aa0c27fc3d705e7ca30650be39e660f5
Hardy <hardytomp@gmail.com>
2020-05-27 11:38:17 +0530

    contigous largest sub array

--- src/leetCode/IntervalList.java
+++ src/leetCode/IntervalList.java
@@ -6,5 +6,35 @@ public class IntervalList {
 		// TODO Auto-generated method stub
 
 	}
+	
+    public int[][] intervalIntersection(int[][] A, int[][] B) {
+        
+        for(int i=0;i < A.length; i++) {
+            System.out.print(A[i][0]+" ");
+            System.out.print(A[i][1]+" ");            
+        }
+        System.out.println();
+        for(int i=0;i < B.length; i++) {
+            System.out.print(B[i][0]+" ");
+            System.out.print(B[i][1]+" ");            
+        }
+        
+        int startA=0;
+        int startB=0;
+        
+        int 
+        
+        while(startA < A.length && startB < B.length) {
+        	
+        	if(A[startA][1] >= B[startB][0]) {
+        		
+        	}
+        	
+        }
+        
+        
+        return A;
+        
+    }	
 
 }
---
+++ src/leetCode/contigousArray.java
@@ -0,0 +1,49 @@
+package leetCode;
+
+import java.util.Stack;
+
+public class contigousArray {
+
+	public static void main(String[] args) {
+		int [] test= {0,1,0,1,2,3,2,1};
+		//int [] test= {0,1};
+		System.out.println(findMaxLength(test));
+	}
+	
+    public static int findMaxLength(int[] nums) {
+    	
+    	Stack<Integer> incrementingStack = new Stack<>();
+    	Stack<Integer> decrementingStack = new Stack<>();
+    	incrementingStack.push(nums[0]);
+    	decrementingStack.push(nums[0]);
+    	int maxArray =1;
+    	
+    	for(int i=1;i<nums.length;i++) {
+    		
+    		maxArray = maxArray > incrementingStack.size()?maxArray:incrementingStack.size();
+    		maxArray = maxArray > decrementingStack.size()?maxArray:decrementingStack.size();
+    		if(!incrementingStack.isEmpty() && nums[i] - incrementingStack.peek() != 1) {
+    			while(!incrementingStack.isEmpty()) {
+    				incrementingStack.pop();
+    			}	
+    		} 
+			incrementingStack.push(nums[i]);
+    		
+    		
+    		if(!decrementingStack.isEmpty()&&decrementingStack.peek()-nums[i] != 1  ) {
+    			while(!decrementingStack.isEmpty()) {
+    				decrementingStack.pop();
+    			}    			
+    		}
+			decrementingStack.push(nums[i]);
+    		
+    		
+    	}
+		maxArray = maxArray > incrementingStack.size()?maxArray:incrementingStack.size();
+		maxArray = maxArray > decrementingStack.size()?maxArray:decrementingStack.size();
+    	
+    	return maxArray;
+    	
+    }
+
+}
